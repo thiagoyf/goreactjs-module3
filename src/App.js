5f36df4f94338a18c06f8ff6a5342f42688ea55f
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-05-24 13:27:56 -0400

    Adding redux

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { Provider } from 'react-redux';
 
 import store from './store';
 
+import TodoList from './TodoList';
+
 const App = () => (
   <Provider store={store}>
-    <h1>Hello World</h1>
+    <TodoList />
   </Provider>
 );
 

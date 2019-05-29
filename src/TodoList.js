a9fbe7514d492c22c236af364a4fabf0f63fef9f
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-05-29 06:50:59 -0400

    Adding feature remove

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,11 +5,16 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as TodoActions from './store/action/todos';
 
-const TodoList = ({ todos, addTodo }) => (
+const TodoList = ({ todos, addTodo, removeTodo }) => (
   <Fragment>
     <ul>
       {todos.map(todo => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          {todo.text}
+          <button type="button" onClick={() => removeTodo(todo.id)}>
+            Remover
+          </button>
+        </li>
       ))}
     </ul>
     <button type="button" onClick={() => addTodo('Novo todo')}>
@@ -19,13 +24,14 @@ const TodoList = ({ todos, addTodo }) => (
 );
 
 TodoList.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+  removeTodo: PropTypes.func.isRequired,
   todos: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
       text: PropTypes.string,
     }),
   ).isRequired,
-  addTodo: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
